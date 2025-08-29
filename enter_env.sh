ed5c085fae99950c9f49cab61aa0a2c30bc5a6f0
Photon <1472411+quantumxt@users.noreply.github.com>
2025-08-30 00:50:00 +0800

    feat: Dynamic build check (#3)
    * chore: Update script location
    * feat: Check hash to determine whether to rebuild docker image

--- enter_env.sh
+++ enter_env.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cd docker
-bash .select_display.sh
+bash docker/.select_display.sh
 CONTAINER_NAME=$(docker ps | grep beluga_ros2 | awk '{print $NF}')
 docker exec -it $CONTAINER_NAME bash
