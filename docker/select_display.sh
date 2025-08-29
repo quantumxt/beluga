14c4ef13f7ac4cc724bacb1f56c310fefbd05e9f
Photon <1472411+quantumxt@users.noreply.github.com>
2025-08-29 21:52:07 +0800

    fix: Select first instance (wayland)

--- docker/select_display.sh
+++ docker/select_display.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USER=$(whoami)
-SESSION_ID=$(loginctl | grep "$USER" | awk '{print $1}')
+SESSION_ID=$(loginctl | grep "$USER" -m 1 | awk '{print $1}')
 SESSION_TYPE=$(loginctl show-session "$SESSION_ID" --property=Type --value)
 
 echo "Detected display manager: $SESSION_TYPE"
