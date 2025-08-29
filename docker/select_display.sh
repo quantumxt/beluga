ee07850aeb757a0d3702b3117fb572875fb69275
quantumxt <1472411+quantumxt@users.noreply.github.com>
2025-08-29 20:27:21 +0700

    feat: Split config based on display manager

---
+++ docker/select_display.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+USER=$(whoami)
+SESSION_ID=$(loginctl | grep "$USER" | awk '{print $1}')
+SESSION_TYPE=$(loginctl show-session "$SESSION_ID" --property=Type --value)
+
+echo "Detected display manager: $SESSION_TYPE"
+
+if [ "$SESSION_TYPE" == "wayland" ]; then
+    docker compose -f docker-compose-wayland.yml up -d
+elif [ "$SESSION_TYPE" == "x11" ]; then
+    docker compose -f docker-compose-x11.yml up -d
+else
+    echo "Unable to detect display manager..."
+    exit 1
+fi
